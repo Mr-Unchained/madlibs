8cc965aa1796f053de18f184a3f4a2e40812cd6e
Mr-Unchained <Mr-Unchained@users.noreply.github.com>
2021-01-21 14:15:45 +0100

    madlibs Generator

---
+++ madlibs generator.py
@@ -0,0 +1,11 @@
+i = 1
+while (1<10):
+    noun = input('Enter  noun' )
+    pronoun = input('Enter pronoun')
+    Adjective = input('Enter')
+    print('this ia a ', noun, 'and this not a pronoun')
+    print(' this is a',pronoun,'or an adjective')
+    print('na adjective be this o', adjective)
+    i += 1
+   
+
